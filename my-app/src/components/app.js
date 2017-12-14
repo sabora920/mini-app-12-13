571d2f5b127da27a0c3db95ca289057ca0ce216b
Robert Frazier <robert.v.frazier@gmail.com>
2017-12-14 11:44:30 -0500

    Finally working the way I want it!

--- my-app/src/components/app.js
+++ my-app/src/components/app.js
@@ -25,24 +25,47 @@ export default class Gifs extends React.Component {
       return (
         <div>
           <form className="buttons-form">
-            {JsonImages.map(function(option,i){
-              console.log(option.description);
+            {JsonImages.map((option,i)=>{
+              return(<div><input type="radio" value={i} 
+              checked={this.state.radioButtonSelected===i+''}
+              onChange={e=>this.handleOptionChange(e)} />{option.description}</div>);
             })}
-            <input type="radio" value="0" 
-              checked={this.state.radioButtonSelected==="0"}
-              onChange={e=>this.handleOptionChange(e)} />
-            {JsonImages[0].description}<br/>
-            <input type="radio" value="1" 
-              checked={this.state.radioButtonSelected==="1"}
-              onChange={e=>this.handleOptionChange(e)} />
-            {JsonImages[1].description}<br/>
-            <input type="radio" value="2" 
-              checked={this.state.radioButtonSelected==="2"}
-              onChange={e=>this.handleOptionChange(e)} />
-            {JsonImages[2].description}<br/>
           </form>
           {image}
         </div>
       )
   }
 }
+
+              // console.log(option.description);
+
+
+            // <input type="radio" value="0" 
+            //   checked={this.state.radioButtonSelected==="0"}
+            //   onChange={e=>this.handleOptionChange(e)} />
+            // {JsonImages[0].description}<br/>
+
+            // <input type="radio" value="1" 
+            //   checked={this.state.radioButtonSelected==="1"}
+            //   onChange={e=>this.handleOptionChange(e)} />
+            // {JsonImages[1].description}<br/>
+
+            // <input type="radio" value="2" 
+            //   checked={this.state.radioButtonSelected==="2"}
+            //   onChange={e=>this.handleOptionChange(e)} />
+            // {JsonImages[2].description}<br/>
+
+            // <input type="radio" value="3" 
+            //   checked={this.state.radioButtonSelected==="3"}
+            //   onChange={e=>this.handleOptionChange(e)} />
+            // {JsonImages[3].description}<br/>
+
+            // <input type="radio" value="4" 
+            //   checked={this.state.radioButtonSelected==="4"}
+            //   onChange={e=>this.handleOptionChange(e)} />
+            // {JsonImages[4].description}<br/>
+
+            // <input type="radio" value="5" 
+            //   checked={this.state.radioButtonSelected==="5"}
+            //   onChange={e=>this.handleOptionChange(e)} />
+            // {JsonImages[5].description}<br/>
